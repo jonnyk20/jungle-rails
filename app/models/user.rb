950d3747e2ed853fb9fd795cb71bbb222b5fc3e7
Jonny Kalambay <jonnyk20@gmail.com>
2017-10-16 21:46:26 +0000

    authenticate_with_credentials method created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
   has_secure_password
   has_many :reviews
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+    @user.authenticate(password)
+  end
 end
